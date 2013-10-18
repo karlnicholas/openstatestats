eb350574669aae79887a528c2cbb1ef39b76800f
Karl Nicholas <Karl.Nicholas@outlook.com>
2013-10-18 15:42:48 -0700

    Changed to use new openstates4j bulkdata api. Added total bills by committee and OfficeScore

--- src/main/java/TotalBillsPassed.java
+++ src/main/java/TotalBillsPassed.java
@@ -1,90 +1,68 @@
-import java.text.SimpleDateFormat;
-import java.util.Enumeration;
 import java.util.TimeZone;
 import java.util.TreeMap;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
 
+import net.thegreshams.openstates4j.bulkdata.Bills;
+import net.thegreshams.openstates4j.bulkdata.Committees;
+import net.thegreshams.openstates4j.bulkdata.Legislature;
+import net.thegreshams.openstates4j.bulkdata.LoadState;
 import net.thegreshams.openstates4j.model.Bill;
 import net.thegreshams.openstates4j.model.Committee;
 import net.thegreshams.openstates4j.model.Legislator;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 public class TotalBillsPassed {
 
-	static class SponsorSuccessStats {
-		int count = 0;
+	static class AuthorSuccessStats {
+		int billCount = 0;
+		int committeeBillCount = 0;
 		int officeScore = 0;
 		Legislator legislator;
-		SponsorSuccessStats(Legislator legislator, int officeScore) {
+		AuthorSuccessStats(Legislator legislator, int officeScore) {
 			this.legislator = legislator;
 			this.officeScore = officeScore;
 		}
 	}
 
 	public static void main(String[] args) throws Exception {
-		ObjectMapper mapper = new ObjectMapper();
-		SimpleDateFormat sdf = new SimpleDateFormat( "yyyy-MM-dd HH:mm:ss" );
-		sdf.setTimeZone(TimeZone.getTimeZone("GMT-08:00"));
-		mapper.setDateFormat( sdf );
 
-		ZipFile zipFile = new ZipFile( TotalBillsPassed.class.getResource("2013-10-07-ca-json.zip").getFile() );
-		TreeMap<String, Committee> committees = readCommittees(mapper, zipFile);
-		TreeMap<String, SponsorSuccessStats> sponsorSuccess = readLegislators(mapper, zipFile, committees);
+		LoadState.Load( TotalBillsPassed.class.getResource("2013-10-07-ca-json.zip").getFile(), TimeZone.getTimeZone("GMT-08:00") );
+		TreeMap<String, AuthorSuccessStats> sponsorSuccess = readLegislators();
 		
-		Enumeration<? extends ZipEntry> entries = zipFile.entries();
-		while ( entries.hasMoreElements() ) {
-			ZipEntry entry = entries.nextElement();
-			if ( entry.isDirectory() ) continue;
-			String eName = entry.getName();
-			if ( eName.contains("ca/20132014")) {
-				Bill bill = readBill(mapper, zipFile, entry);
-				boolean passed = determinePassed(bill);
-				if ( passed ) {
-					String key = determinePrincipalSponsor(bill);
-					if ( key != null ) {
-						boolean cFlag = false;
-						SponsorSuccessStats sponsorStats = sponsorSuccess.get(key);
-						if ( sponsorStats == null ) {
-							key = findCommitteeKey(committees, key, bill.chamber);
-							if ( key != null ) {
-								Committee committee = committees.get(key);
-								if ( committee != null ) {
-									for ( Committee.Member member: committee.members ) {
-										if ( member.role.equals("Chair")) {
-											sponsorStats = sponsorSuccess.get( member.legislatorId );
-											cFlag = true;
-											break;
-										}
+		for ( Bill bill: Bills.bills() ) {
+			boolean passed = determinePassed(bill);
+			if ( passed ) {
+				String key = determinePrincipalSponsor(bill);
+				if ( key != null ) {
+					boolean cFlag = false;
+					AuthorSuccessStats sponsorStats = sponsorSuccess.get(key);
+					if ( sponsorStats == null ) {
+						key = Committees.findCommitteeKey(key, bill.chamber);
+						if ( key != null ) {
+							Committee committee = Committees.get(key);
+							if ( committee != null ) {
+								for ( Committee.Member member: committee.members ) {
+									if ( member.role.toLowerCase().equals("chair")) {
+										sponsorStats = sponsorSuccess.get( member.legislatorId );
+										cFlag = true;
+										break;
 									}
 								}
 							}
 						}
-						if ( sponsorStats != null && !cFlag ) sponsorStats.count++;
-						else if ( !cFlag) System.out.println("Principal Sponsor Not Found:" + bill.sponsors );
-					} else {
-						System.out.println("Principal Sponsor Not Found:" + bill.sponsors ); 
 					}
+					if ( sponsorStats != null && !cFlag ) sponsorStats.billCount++;
+					else if ( sponsorStats != null && cFlag ) sponsorStats.committeeBillCount++;
+					else if ( !cFlag) System.out.println("Principal Sponsor Not Found:" + bill.sponsors );
+				} else {
+					System.out.println("Principal Sponsor Not Found:" + bill.sponsors ); 
 				}
-				continue;
 			}
+			continue;
 		}
-		zipFile.close();
-		System.out.println( "NAME" + "\t" + "CHAMBER" + "\t" + "DISTRICT" + "\t" + "PARTY" + "\t" + "OFFICESCORE" + "\t" + "BILLSCHAPTERED"  );
-		for ( SponsorSuccessStats sponsorStats: sponsorSuccess.values() ) {
+		System.out.println( "NAME" + "\t" + "CHAMBER" + "\t" + "DISTRICT" + "\t" + "PARTY" + "\t" + "OFFICESCORE" + "\t" + "BILLS" + "\t" + "COMMITTEEBILLS"  );
+		for ( AuthorSuccessStats sponsorStats: sponsorSuccess.values() ) {
 			Legislator legislator = sponsorStats.legislator;
-			System.out.println( legislator.fullName + "\t" + legislator.chamber + "\t" + legislator.district + "\t" + legislator.party + "\t" + sponsorStats.officeScore + "\t" + sponsorStats.count  );
+			System.out.println( legislator.fullName + "\t" + legislator.chamber + "\t" + legislator.district + "\t" + legislator.party + "\t" + sponsorStats.officeScore + "\t" + sponsorStats.billCount + "\t" + sponsorStats.committeeBillCount );
 		}
-	}
-	
-	private static String findCommitteeKey(TreeMap<String, Committee> committees, String namePart, String chamber) {
-		String key = null;
-		for ( Committee committee: committees.values() ) {
-			if ( namePart.contains(committee.committee) && !namePart.equals(committee.committee) ) System.out.println(namePart);
-			if ( (committee.committee.contains(namePart) || namePart.contains(committee.committee)) && (committee.chamber.equals(chamber) || committee.chamber.equals("joint")) ) return committee.id;
-		}
-		return key;
 	}
 	
 	private static String determinePrincipalSponsor(Bill bill) {
@@ -101,58 +79,20 @@ public class TotalBillsPassed {
 	private static boolean determinePassed(Bill bill) {
 		boolean passed = false;
 		for ( Bill.Action action: bill.actions ) {
-			if ( action.action.contains("Chaptered") ) return true;
+			if ( action.action.toLowerCase().contains("chaptered") ) return true;
 		}
 		return passed;
 	}
 	
-	private static TreeMap<String, SponsorSuccessStats> readLegislators(ObjectMapper mapper, ZipFile zipFile, TreeMap<String, Committee> committees) throws Exception {
-		TreeMap<String, SponsorSuccessStats> legislators = new TreeMap<>();
-		Enumeration<? extends ZipEntry> entries = zipFile.entries();
-		while ( entries.hasMoreElements() ) {
-			ZipEntry entry = entries.nextElement();
-			if ( entry.isDirectory() ) continue;
-			String eName = entry.getName();
-			if ( eName.contains("legislators")) {
-				Legislator legislator = readLegislator(mapper, zipFile, entry);
-				
-				if ( legislator.isActive ) {
-					int officeScore = determineOfficeScore(legislator, committees);
-					legislators.put(legislator.id, new SponsorSuccessStats(legislator, officeScore));
-				}
+	private static TreeMap<String, AuthorSuccessStats> readLegislators() throws Exception {
+		TreeMap<String, AuthorSuccessStats> legislators = new TreeMap<>();
+		for ( Legislator legislator: Legislature.legislators()) {
+			if ( legislator.isActive ) {
+				int officeScore = determineOfficeScore(legislator);
+				legislators.put(legislator.id, new AuthorSuccessStats(legislator, officeScore));
 			}
 		}
 		return legislators;
-	}
-	
-	private static Legislator readLegislator(ObjectMapper mapper, ZipFile zipFile, ZipEntry entry ) throws Exception {
-		Legislator legislator = mapper.readValue( zipFile.getInputStream(entry), Legislator.class );
-		return legislator;
-	}
-	
-	private static Bill readBill(ObjectMapper mapper, ZipFile zipFile, ZipEntry entry ) throws Exception {
-		Bill bill  = mapper.readValue( zipFile.getInputStream(entry), Bill.class );
-		return bill;
-	}
-	
-	private static TreeMap<String, Committee> readCommittees(ObjectMapper mapper, ZipFile zipFile) throws Exception {
-		TreeMap<String, Committee> committees = new TreeMap<>();
-		Enumeration<? extends ZipEntry> entries = zipFile.entries();
-		while ( entries.hasMoreElements() ) {
-			ZipEntry entry = entries.nextElement();
-			if ( entry.isDirectory() ) continue;
-			String eName = entry.getName();
-			if ( eName.contains("committees")) {
-				Committee committee = readCommittee(mapper, zipFile, entry);
-				committees.put(committee.id, committee);
-			}
-		}
-		return committees;
-	}
-	
-	private static Committee readCommittee(ObjectMapper mapper, ZipFile zipFile, ZipEntry entry ) throws Exception {
-		Committee committee = mapper.readValue( zipFile.getInputStream(entry), Committee.class );
-		return committee;
 	}
 	
 	/**
@@ -170,13 +110,13 @@ public class TotalBillsPassed {
 	 * Added -1 if no office held
 	 * 
 	 */
-	private static int determineOfficeScore(Legislator legislator, TreeMap<String, Committee> committees) {
+	private static int determineOfficeScore(Legislator legislator) {
 		int score = -1;
 		for ( Legislator.Role role: legislator.roles ) {
 			String roleType = role.type.toLowerCase();
 			if ( role.committee != null ) {
-				String key = findCommitteeKey(committees, role.committee.committee, legislator.chamber);
-				Committee committee = committees.get( key );
+				String key = Committees.findCommitteeKey(role.committee.committee, legislator.chamber);
+				Committee committee = Committees.get( key );
 				String nRoleType = findCommitteeRole(committee, legislator);
 				if ( nRoleType != null && nRoleType.equals("III") ) System.out.println(committee.committee); 
 				if ( nRoleType != null ) roleType = nRoleType.toLowerCase();
